9be08631824eff8dfb9981c1b856841a560bdfcb
hk <kiptooharon.hk@gmail.com>
2024-04-02 22:38:43 +0300

    agent node configuration with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,73 @@
+#web server puppet manifest
+
+$s_content = "#server configuration
+
+server {
+	#listening ports and ip all ips
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	#server root directory
+        root /var/www/html;
+
+	#server name
+	server_name _;
+
+	#server custome header
+	add_header X_Server_By \$hostname;
+
+	#error page
+	error_page 404 /404.html;
+
+	#server redirect block
+	location /redirect_me/{
+		return 301;
+	}
+
+	#server home
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+}
+"
+
+$landing_page = "Hello World!\n"
+$error_page = "Ceci n'est pas une page\n"
+
+package {'nginx':
+  ensure => installed,
+}
+
+file {'config':
+  ensure  => 'file',
+  path    => '/etc/nginx/sites-available/default',
+  require => Package['nginx'],
+  content => $s_content,
+  notify  => Service['nginx'],
+}
+
+file { 'data directory':
+  ensure  => 'directory',
+  path    => '/var/www/html',
+  require => Package['nginx']
+}
+
+file {'page':
+  ensure  => 'file',
+  path    => '/var/www/html/index.html',
+  content => $landing_page,
+  require => Package['nginx'],
+}
+
+file {'error':
+  ensure  => 'file',
+  path    => '/var/www/html/404.html',
+  content => $error_page,
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service {'nginx':
+  ensure => 'running',
+  enable => 'true',
+}
