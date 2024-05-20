5ba888be09624c7800a5b678300f4330a6f1b995
hk <kiptooharon.hk@gmail.com>
2024-05-21 01:37:10 +0300

    Gathering data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+using rest api to fetch info about employess
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+
+    employee = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
+
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    for complete in completed:
+        print("\t {}".format(complete))
---
+++ 0x15-api/README.md
@@ -0,0 +1,5 @@
+# REST API
+
+## 0-gather_data_from_an_API.py
+   using this REST API, for a given employee ID, to returns information about his/her TODO list progress
+<author><kiptooharon.hk@ggmail.com>
